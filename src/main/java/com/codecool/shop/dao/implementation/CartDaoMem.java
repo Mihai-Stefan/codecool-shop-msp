3daa8c15d889b2d5ea290dc9b128676465d71b7e
grk-0-1 <cristina.grecu12@gmail.com>
2021-01-05 11:14:40 +0200

    DAO in memory implementation for cart

---
+++ src/main/java/com/codecool/shop/dao/implementation/CartDaoMem.java
@@ -0,0 +1,45 @@
+package com.codecool.shop.dao.implementation;
+
+import com.codecool.shop.dao.CartDao;
+import com.codecool.shop.model.User;
+import com.codecool.shop.model.cart.Cart;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.stream.Collectors;
+
+public class CartDaoMem implements CartDao {
+
+    private List<Cart> data = new ArrayList<>();
+    private static CartDaoMem instance = null;
+
+    private CartDaoMem() {}
+
+    public static CartDaoMem getInstance() {
+        if (instance == null) {
+            instance = new CartDaoMem();
+        }
+        return instance;
+    }
+
+    @Override
+    public void add(Cart cart) {
+        cart.setId(data.size() + 1);
+        data.add(cart);
+    }
+
+    @Override
+    public Cart find(int id) {
+        return data.stream().filter(t -> t.getId() == id).findFirst().orElse(null);
+    }
+
+    @Override
+    public void remove(int id) {
+        data.remove(find(id));
+    }
+
+    @Override
+    public List<Cart> getBy(User user) {
+        return data.stream().filter(t -> t.getUser().equals(user)).collect(Collectors.toList());
+    }
+}
