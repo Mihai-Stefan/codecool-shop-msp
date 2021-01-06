9954456532cd8dc83720ae47d63c6d53cd403566
danamusetescu <danactt27@gmail.com>
2021-01-06 16:23:37 +0200

    * Updated order Model

--- src/main/java/com/codecool/shop/model/order/Order.java
+++ src/main/java/com/codecool/shop/model/order/Order.java
@@ -1,7 +1,12 @@
 package com.codecool.shop.model.order;
 
+import com.codecool.shop.model.User;
 import com.codecool.shop.model.cart.Cart;
 
 public class Order {
-    Cart cart;
+   private Cart cart;
+   private User user;
+   private Address billingAddress;
+   private Address shippingAddress;
+
 }
