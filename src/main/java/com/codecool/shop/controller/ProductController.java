a872570f6fc99c85e0a3da184963b1d4b649c539
Mihai-Stefan <mihaiste@gmail.com>
2021-01-06 19:06:24 +0200

    list all, filter by category and supplier

--- src/main/java/com/codecool/shop/controller/ProductController.java
+++ src/main/java/com/codecool/shop/controller/ProductController.java
@@ -2,11 +2,14 @@ package com.codecool.shop.controller;
 
 import com.codecool.shop.dao.ProductCategoryDao;
 import com.codecool.shop.dao.ProductDao;
+import com.codecool.shop.dao.SupplierDao;
 import com.codecool.shop.dao.UserDao;
 import com.codecool.shop.dao.implementation.ProductCategoryDaoMem;
 import com.codecool.shop.dao.implementation.ProductDaoMem;
 import com.codecool.shop.config.TemplateEngineUtil;
+import com.codecool.shop.dao.implementation.SupplierDaoMem;
 import com.codecool.shop.dao.implementation.UserDaoMem;
+import com.codecool.shop.model.Supplier;
 import org.thymeleaf.TemplateEngine;
 import org.thymeleaf.context.WebContext;
 
@@ -26,19 +29,46 @@ public class ProductController extends HttpServlet {
     protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
         ProductDao productDataStore = ProductDaoMem.getInstance();
         ProductCategoryDao productCategoryDataStore = ProductCategoryDaoMem.getInstance();
+        SupplierDao supplierDao = SupplierDaoMem.getInstance();
         UserDao userDataStore = UserDaoMem.getInstance();
 
         TemplateEngine engine = TemplateEngineUtil.getTemplateEngine(req.getServletContext());
         WebContext context = new WebContext(req, resp, req.getServletContext());
-        context.setVariable("category", productCategoryDataStore.find(1));
-        context.setVariable("products", productDataStore.getBy(productCategoryDataStore.find(1)));
-        context.setVariable("user", userDataStore.find(1));
-        // // Alternative setting of the template context
-        // Map<String, Object> params = new HashMap<>();
-        // params.put("category", productCategoryDataStore.find(1));
-        // params.put("products", productDataStore.getBy(productCategoryDataStore.find(1)));
-        // context.setVariables(params);
+
+        //read from form
+        String cattegoryId = req.getParameter("category-id");
+//        String supplierId = req.getParameter("supplier-id");
+        int selectedCat = 0;
+
+        if(cattegoryId != null)
+            selectedCat = Integer.parseInt(cattegoryId);
+
+//        context.setVariable("category", productCategoryDataStore.find(4));
+//        context.setVariable("products", productDataStore.getBy(productCategoryDataStore.find(4)));
+//        context.setVariable("user", userDataStore.find(1));
+
+
+
+         // Alternative setting of the template context
+        Map<String, Object> params = new HashMap<>();
+
+        //== Case List All ==
+        if( cattegoryId == null || selectedCat == 0){
+            params.put("category", "All");
+            params.put("products", productDataStore.getAll());
+        }
+        else{
+            params.put("category", productCategoryDataStore.find(selectedCat).getName());
+
+//            params.put("products", productDataStore.getBy(productCategoryDataStore.find(selectedCat)));
+            params.put("products", productDataStore.getBy(productCategoryDataStore.find(selectedCat), supplierDao.find(3)));
+
+        }
+
+
+
+        context.setVariables(params);
         engine.process("product/index.html", context, resp.getWriter());
-    }
+        }
 
 }
