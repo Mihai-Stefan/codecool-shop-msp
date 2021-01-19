b94bd4d939f111224af5d8254c9e9121ced2f692
danamusetescu <danactt27@gmail.com>
2021-01-19 12:31:44 +0200

    added ProductCategoryDaoMemTest added ProductDaoMemTest added SupplierDaoMemTest to test directory

---
+++ src/test/java/com/codecool/shop/dao/implementation/ProductCategoryDaoMemTest.java
@@ -0,0 +1,34 @@
+package com.codecool.shop.dao.implementation;
+
+import org.junit.jupiter.api.AfterEach;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class ProductCategoryDaoMemTest {
+
+    @BeforeEach
+    void setUp() {
+    }
+
+    @AfterEach
+    void tearDown() {
+    }
+
+    @Test
+    void add() {
+    }
+
+    @Test
+    void find() {
+    }
+
+    @Test
+    void remove() {
+    }
+
+    @Test
+    void getAll() {
+    }
+}
---
+++ src/test/java/com/codecool/shop/dao/implementation/ProductDaoMemTest.java
@@ -0,0 +1,36 @@
+package com.codecool.shop.dao.implementation;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class ProductDaoMemTest {
+
+    @Test
+    void add() {
+    }
+
+    @Test
+    void find() {
+    }
+
+    @Test
+    void remove() {
+    }
+
+    @Test
+    void getAll() {
+    }
+
+    @Test
+    void getBy() {
+    }
+
+    @Test
+    void testGetBy() {
+    }
+
+    @Test
+    void testGetBy1() {
+    }
+}
