2f3240dcfb6b1130d62edfcb3481a4b965cab2ea
danamusetescu <danactt27@gmail.com>
2021-01-20 12:38:14 +0200

    implemented getAll, getBySupplier, getByCategory and getByproductCategoryAndSupplier methods success case bug fix

--- src/test/java/com/codecool/shop/dao/implementation/ProductDaoTest.java
+++ src/test/java/com/codecool/shop/dao/implementation/ProductDaoTest.java
@@ -30,9 +30,7 @@ class ProductDaoTest {
 //        assertion
         List<Product> expectedValue = new ArrayList<>();
         expectedValue.add(productTest);
-        System.out.println(expectedValue);
         List<Product> actualValue = productDao.getAll();
-        System.out.println(actualValue);
 
         assertEquals(expectedValue, actualValue);
 
@@ -51,9 +49,13 @@ class ProductDaoTest {
 
     productDao.add(productTest);
 
+    List<Product> expectedList = new ArrayList<>();
+    expectedList.add(productTest);
+    Product expectedValue = productDao.getAll().get(0);
+
     int productId = productTest.getId();
     Product actualValue = productDao.find(productId);
-    Product expectedValue = productDao.getAll().get(0);
+
     assertEquals(expectedValue, actualValue);
 
     productDao.remove(productTest.getId());
@@ -80,18 +82,86 @@ class ProductDaoTest {
     }
 
     @Test
-    void getAll() {
+    void testGetAllProductsMethodSuccess() {
+
+        ProductDao productDao = ProductDaoMem.getInstance();
+        var apple = new Supplier("Apple Computers", "Smartphones, laptops and computers");
+        var laptop = new ProductCategory("Laptop", "Hardware", "A laptop or laptop computer, is a small, portable personal computer (PC) with a \"clamshell\" form factor.");
+        var productTest = new Product("MacBook Pro", 1500, "USD", "Apple M1 Chip with 8-Core CPU and 8-Core GPU\n" +
+                "512GB Storage", laptop, apple);
+
+        productDao.add(productTest);
+
+        List<Product> expectedValue = new ArrayList<>();
+        expectedValue.add(productTest);
+        List<Product> actualValue = productDao.getAll();
+
+        assertEquals(expectedValue, actualValue);
+
+        productDao.remove(productTest.getId());
+
     }
 
     @Test
-    void getBy() {
+    void testGetBySupplierMethodSuccess() {
+        ProductDao productDao = ProductDaoMem.getInstance();
+        var apple = new Supplier("Apple Computers", "Smartphones, laptops and computers");
+        var laptop = new ProductCategory("Laptop", "Hardware", "A laptop or laptop computer, is a small, portable personal computer (PC) with a \"clamshell\" form factor.");
+        var productTest = new Product("MacBook Pro", 1500, "USD", "Apple M1 Chip with 8-Core CPU and 8-Core GPU\n" +
+                "512GB Storage", laptop, apple);
+
+        productDao.add(productTest);
+
+        List<Product> expectedValue = new ArrayList<>();
+        expectedValue.add(productTest);
+        List<Product> actualValue = productDao.getBy(apple);
+
+        assertEquals(expectedValue, actualValue);
+
+        productDao.remove(productTest.getId());
     }
 
     @Test
-    void testGetBy() {
+    void testGetByCategoryMethodSuccess() {
+        ProductDao productDao = ProductDaoMem.getInstance();
+        var apple = new Supplier("Apple Computers", "Smartphones, laptops and computers");
+        var laptop = new ProductCategory("Laptop", "Hardware", "A laptop or laptop computer, is a small, portable personal computer (PC) with a \"clamshell\" form factor.");
+        var productTest = new Product("MacBook Pro", 1500, "USD", "Apple M1 Chip with 8-Core CPU and 8-Core GPU\n" +
+                "512GB Storage", laptop, apple);
+
+        productDao.add(productTest);
+
+        List<Product> expectedValue = new ArrayList<>();
+        expectedValue.add(productTest);
+        List<Product> actualValue = productDao.getBy(laptop);
+
+        assertEquals(expectedValue, actualValue);
+
+        productDao.remove(productTest.getId());
+
     }
 
     @Test
-    void testGetBy1() {
+    void testGetByProductCategoryAndSupplierMethodSuccess() {
+        ProductDao productDao = ProductDaoMem.getInstance();
+        var apple = new Supplier("Apple Computers", "Smartphones, laptops and computers");
+        var laptop = new ProductCategory("Laptop", "Hardware", "A laptop or laptop computer, is a small, portable personal computer (PC) with a \"clamshell\" form factor.");
+        var productTest1 = new Product("MacBook Pro", 1500, "USD", "Apple M1 Chip with 8-Core CPU and 8-Core GPU\n" +
+                "512GB Storage", laptop, apple);
+        var amazon = new Supplier("Amazon", "Digital content and services");
+        var tablet = new ProductCategory("Tablet", "Hardware", "A tablet computer, commonly shortened to tablet, is a thin, flat mobile computer with a touchscreen display.");
+        var productTest2 = new Product("Amazon Fire", 49.9f, "USD", "Fantastic price. Large content ecosystem. Good parental controls. Helpful technical support.", tablet, amazon);
+        productDao.add(productTest1);
+        productDao.add(productTest2);
+
+        List<Product> expectedValue = new ArrayList<>();
+        expectedValue.add(productTest2);
+
+        List<Product> actualValue = productDao.getBy(tablet, amazon);
+
+        assertEquals(expectedValue, actualValue);
+
+        productDao.remove(productTest1.getId());
+        productDao.remove(productTest2.getId());
     }
 }
