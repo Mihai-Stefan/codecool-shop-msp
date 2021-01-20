4e372ab13c6b03958ac95f87c95532bd014337c2
danamusetescu <danactt27@gmail.com>
2021-01-20 18:17:55 +0200

    implemented remove, find, getAll ProductCategory method success case refactor test file name for

---
+++ src/test/java/com/codecool/shop/dao/implementation/ProductCategoryDaoTest.java
@@ -0,0 +1,82 @@
+package com.codecool.shop.dao.implementation;
+
+import com.codecool.shop.dao.ProductCategoryDao;
+import com.codecool.shop.model.ProductCategory;
+import org.junit.jupiter.api.AfterEach;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class ProductCategoryDaoTest {
+
+    @Test
+    void testAddProductCategoryMethodSuccess() {
+        ProductCategoryDao categoryDao = ProductCategoryDaoMem.getInstance();
+        ProductCategory smartphone = new ProductCategory("Smartphone", "Phone", "A phone ");
+
+        categoryDao.add(smartphone);
+
+        List<ProductCategory> expectedValue = new ArrayList<>();
+        expectedValue.add(smartphone);
+        List<ProductCategory> actualValue = categoryDao.getAll();
+
+        assertEquals(expectedValue, actualValue);
+
+        categoryDao.remove(smartphone.getId());
+    }
+
+    @Test
+    void testFindProductCategoryMethodSuccess() {
+        ProductCategoryDao categoryDao = ProductCategoryDaoMem.getInstance();
+        ProductCategory smartphone = new ProductCategory("Smartphone", "Phone", "A phone ");
+
+        categoryDao.add(smartphone);
+
+        List<ProductCategory> expectedCategories = new ArrayList<>();
+        expectedCategories.add(smartphone);
+        ProductCategory expectedValue = categoryDao.getAll().get(0);
+        int productCategoryId = smartphone.getId();
+        ProductCategory actualValue = categoryDao.find(productCategoryId);
+
+        assertEquals(expectedValue, actualValue);
+
+        categoryDao.remove(smartphone.getId());
+    }
+
+    @Test
+    void remove() {
+        ProductCategoryDao categoryDao = ProductCategoryDaoMem.getInstance();
+        ProductCategory smartphone = new ProductCategory("Smartphone", "Phone", "A phone ");
+        categoryDao.add(smartphone);
+        int productCategoryId = smartphone.getId();
+
+        categoryDao.remove(productCategoryId);
+
+        List<ProductCategory> expectedValue = new ArrayList<>();
+        List<ProductCategory> actualValue = categoryDao.getAll();
+
+        assertEquals(expectedValue, actualValue);
+
+        categoryDao.remove(smartphone.getId());
+    }
+
+    @Test
+    void getAll() {
+        ProductCategoryDao categoryDao = ProductCategoryDaoMem.getInstance();
+        ProductCategory smartphone = new ProductCategory("Smartphone", "Phone", "A phone ");
+
+        categoryDao.add(smartphone);
+
+        List<ProductCategory> expectedValue = new ArrayList<>();
+        expectedValue.add(smartphone);
+        List<ProductCategory> actualValue = categoryDao.getAll();
+
+        assertEquals(expectedValue, actualValue);
+
+        categoryDao.remove(smartphone.getId());
+    }
+}
